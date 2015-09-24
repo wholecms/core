83d5de6cfb65bc4823d4dc9023fcc34d8b122cab
Furkan Çelik <furkan.celik32@gmail.com>
2015-09-24 23:24:03 +0300

    layouts tr dil dosyasi eklendi

---
+++ translations/whole/tr/layouts.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'profile' => 'Profilim',
+    'logout' => 'Çıkış Yap',
+	'dashboard' => 'Yönetim Paneli',
+	'preview' => 'Siteyi Önizle',
+	'user_operations' => 'Kullanıcı İşlemleri',
+	'users' => 'Kullanıcılar',
+	'user_roles' => 'Kullanıcı Grupları',
+	'permitted_page' => 'Sayfa Erişim Seviyeleri',
+	'search_table' => 'Tabloda Ara',
+	'want_to_delete' => 'Silmek İstediğinize Eminmisiniz?',
+	'not_allowed_title' => 'Whole CMS Yönetim Paneli',
+	'not_access' => 'Erişim İzni Yok',
+	'not_access_message' => 'Bu Sayfayı Görebilmeniz İçin Sayfa Erişim İzniniz Bulunması Gerekmektedir. <br /> Site Yöneticisinden Erişim İzni Talep Edebilirsiniz',
+
+];
