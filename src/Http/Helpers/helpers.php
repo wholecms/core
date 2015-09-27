96679c81d545aa0a78707edb8cc0cac1ab2cb304
Furkan Çelik <furkan.celik32@gmail.com>
2015-09-28 01:53:06 +0300

    helpers coklu dil dosyasi eklendi

--- src/Http/Helpers/helpers.php
+++ src/Http/Helpers/helpers.php
@@ -12,9 +12,19 @@ if ( ! function_exists('day_localize'))
 
     function day_localize($day,$is="en",$localize="tr")
     {
+        $localize = \App::getLocale();
 
-        $days['tr']['D'] = ['Pazartesi','Salı','Çarşamba','Perşembe','Cuma','Cumartesi','Pazar'];
+        $days['locale']['D'] = [
+            trans('whole::http.helpers.monday'),
+            trans('whole::http.helpers.tuesday'),
+            trans('whole::http.helpers.wednesday'),
+            trans('whole::http.helpers.thursday'),
+            trans('whole::http.helpers.friday'),
+            trans('whole::http.helpers.saturday'),
+            trans('whole::http.helpers.sunday'),
+        ];
+
         $days['en']['D'] = ['Monday ','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
-        return $days[$localize]['D'][array_search($day,$days[$is]['D'])];
+        return $days['locale']['D'][array_search($day,$days[$is]['D'])];
     }
 }
---
+++ translations/whole/tr/http/helpers.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+	'monday'=>'Pazartesi',
+	'tesday'=>'Salı',
+	'wednesday'=>'Çarşamba',
+	'thursday'=>'Perşembe',
+	'friday'=>'Cuma',
+	'saturday'=>'Cumartesi',
+	'sunday'=>'Pazar',
+];
