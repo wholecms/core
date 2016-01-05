2b3eb008531ad2ad92e929e91d3c1e23f03ff79e
unknown <furkan.celik32@gmail.com>
2016-01-05 16:42:26 +0200

    sayfa ayarlari icin render eklendi

--- src/Injections/SettingsInjection.php
+++ src/Injections/SettingsInjection.php
@@ -9,14 +9,16 @@
 namespace Whole\Core\Injections;
 use Whole\Core\Repositories\Setting\SettingRepository;
 use Illuminate\Support\Facades\Cache;
+use Whole\Core\Render\PageRender;
 
 class SettingsInjection
 {
     protected $setting;
-
-    public function __construct(SettingRepository $setting)
+    protected $page_render;
+    public function __construct(SettingRepository $setting, PageRender $page_render)
     {
         $this->setting = $setting;
+        $this->page_render = $page_render;
     }
 
     public function get()
@@ -25,10 +27,20 @@ class SettingsInjection
         {
             Cache::rememberForever('settings', function()
             {
-                return $this->setting->first();
+                $setting = $this->setting->first();
+                $setting->title = $this->page_render->reContent($setting->title);
+                $setting->meta_keywords = $this->page_render->reContent($setting->meta_keywords);
+                $setting->meta_description = $this->page_render->reContent($setting->meta_description);
+                $setting->rss_title = $this->page_render->reContent($setting->rss_title);
+                $setting->rss_description = $this->page_render->reContent($setting->rss_description);
+                $setting->offline_message = $this->page_render->reContent($setting->offline_message);
+                $setting->logo_title = $this->page_render->reContent($setting->logo_title);
+                $setting->logo_description = $this->page_render->reContent($setting->logo_description);
+                $setting->copyright = $this->page_render->reContent($setting->copyright);
+
+                return $setting;
             });
         }
         return Cache::get('settings');
-
     }
 }
