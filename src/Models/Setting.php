8e37d2ce301528f5317eac7ed6a5c0897ff385fd
unknown <furkan.celik32@gmail.com>
2016-01-06 01:59:19 +0200

    setting yeni sutunar model fillable eklendi

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    protected $fillable = ['title','meta_description','meta_keywords','rss_description','rss_title','status','offline_message','template_id','logo','logo_title','logo_description','favicon','copyright','google_analytics'];
+    protected $fillable = ['title','meta_description','meta_keywords','rss_description','rss_title','status',
+        'offline_message','template_id','logo','logo_title','logo_description','favicon','copyright',
+        'google_analytics','admin_title','admin_footer','admin_logo','admin_login_logo'];
 
     public function template()
     {
